860b9f48a617edfa6c14653340d98c3239ddf8ef
Naman Kumar Singh <156122185+NKSG100@users.noreply.github.com>
2025-01-06 02:17:14 +0530

    Add files via upload

---
+++ src/app/welcome/page.js
@@ -0,0 +1,22 @@
+'use client';
+import Link from "next/link";
+
+const WelcomePage = () => {
+  return (
+    <div className="w-full max-w-3xl bg-white/10 backdrop-blur-lg rounded-lg p-6 shadow-lg text-center">
+      <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-indigo-400 mb-4">
+        Welcome to the Quiz App
+      </h1>
+      <p className="text-lg mb-6">
+        Test your knowledge on various subjects and have fun while learning!
+      </p>
+      <Link href="/selectSubject">
+        <button className="px-6 py-3 bg-gradient-to-r from-pink-500 to-indigo-500 rounded-full text-lg font-medium shadow-md hover:scale-105 transform transition">
+          Start Quiz
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default WelcomePage;
