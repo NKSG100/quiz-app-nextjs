029c008106c3be7d3502c90453ec397ddc81ab76
Naman Kumar Singh <156122185+NKSG100@users.noreply.github.com>
2025-01-12 02:25:40 +0530

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}', // Make sure this is set to your file structure
+    './src/**/*.{js,ts,jsx,tsx}', // Ensure your file paths are correct
   ],
   theme: {
     extend: {
@@ -14,7 +14,7 @@ module.exports = {
       },
       boxShadow: {
         'lg-custom': '0 8px 15px rgba(0, 0, 0, 0.2)',
-        // Add custom glow effect
+        // Define a custom glow effect
         'glow': '0 0 10px rgba(255, 255, 255, 0.8), 0 0 20px rgba(255, 255, 255, 0.6)',
       },
     },
