46e1e4778035718bcb8dd17ec9c9277892d148ef
Naman Kumar Singh <156122185+NKSG100@users.noreply.github.com>
2025-01-12 19:14:59 +0530

    Update page.js

--- src/app/welcome/page.js
+++ src/app/welcome/page.js
@@ -4,10 +4,10 @@ import Link from "next/link";
 const WelcomePage = () => {
   return (
     <div className="w-full max-w-4xl max-h-5xl bg-white/10 backdrop-blur-lg rounded-lg p-6 shadow-lg text-center transform scale-118 will-change-transform">
-      <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-indigo-400 mb-4">
+      <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-indigo-400 mb-4 antialiased">
         Welcome to the Quiz App
       </h1>
-      <p className="text-lg mb-6">
+      <p className="text-lg mb-6 antialiased">
         Test your knowledge on various subjects and have fun while learning!
       </p>
       <Link href="/selectSubject">
